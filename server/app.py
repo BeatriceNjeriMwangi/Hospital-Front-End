3f70e0aec927aed3ff0a3957f095de4a30cd13c7
beatricenjerimwangi <bbeatricemwangi@gmail.com>
2024-02-08 11:10:50 -0500

    added proxy

--- server/app.py
+++ server/app.py
@@ -2,11 +2,11 @@ from flask import Flask,jsonify,request,make_response
 from flask_migrate import Migrate
 from flask_restful import Api,Resource
 from models import db,Doctor,Patient,Appointment,Treatment
-
+from flask_cors import CORS
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///hospital.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
-
+CORS(app)
 migrate=Migrate(app,db)
 db.init_app(app)
 api=Api(app)
