13474f143d8999b084dfea52fcd3f27abf472772
shalom-nganga <minnomacharia@gmail.com>
2024-02-09 15:33:48 +0300

    made changes to the components in the react app

--- client/src/components/Doctor.js
+++ client/src/components/Doctor.js
@@ -5,7 +5,7 @@ import FormDoctor from '../components/FormDoctor';
 function Doctor() {
   const [doctors, setDoctors] = useState([]);
 
-  const fetchDoctors = async () => {
+  const fetchPatients = async () => {
     try {
       const response = await axios.get('/doctors');
       setDoctors(response.data.doctors);
@@ -15,7 +15,7 @@ function Doctor() {
   };
 
   useEffect(() => {
-    fetchDoctors();
+    fetchPatients();
   }, []);
 
   return (
@@ -26,28 +26,87 @@ function Doctor() {
         <thead>
           <tr>
             <th>ID</th>
-             <th>Name</th>
+            <th>First Name</th>
+            <th>Last Name</th>
             <th>Email</th>
-            <th>DepartmentID</th>
-            <th>schedule</th>
+            <th>Phone Number</th>
+            <th>Registration Number</th>
+            <th>Gender</th>
           </tr>
         </thead>
         <tbody>
           {doctors.map((doctor) => (
             <tr key={doctor.id}>
               <td>{doctor.id}</td>
-              <td>{doctor.name}</td>
+              <td>{doctor.fname}</td>
+              <td>{doctor.lname}</td>
               <td>{doctor.email}</td>
-              <td>{doctor.departmentid}</td>
-              <td>{doctor.schedule}</td>
+              <td>{doctor.phone_number}</td>
+              <td>{doctor.regNo}</td>
+              <td>{doctor.gender}</td>
             </tr>
           ))}
         </tbody>
       </table>
 
-  <FormDoctor/>
-</div>
+      <FormDoctor/>
+    </div>
   );
 }
 
 export default Doctor;
+
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import FormDoctor from '../components/FormDoctor';
+
+// function Doctor() {
+//   const [doctors, setDoctors] = useState([]);
+
+//   const fetchDoctors = async () => {
+//     try {
+//       const response = await axios.get('/doctors');
+//       setDoctors(response.data.doctors);
+//     } catch (error) {
+//       console.error('Error fetching patients:', error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchDoctors();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Doctor List</h1>
+      
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>ID</th>
+//              <th>Name</th>
+//             <th>Email</th>
+//             <th>DepartmentID</th>
+//             <th>schedule</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {doctors.map((doctor) => (
+//             <tr key={doctor.id}>
+//               <td>{doctor.id}</td>
+//               <td>{doctor.name}</td>
+//               <td>{doctor.email}</td>
+//               <td>{doctor.departmentid}</td>
+//               <td>{doctor.schedule}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+
+//   <FormDoctor/>
+// </div>
+//   );
+// }
+
+// export default Doctor;
